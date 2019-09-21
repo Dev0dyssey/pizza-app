1515140c40a7a8049f4b0a765f1cd46fe47da453
ThomasMartinUK <tmuk@protonmail.com>
2019-09-21 11:10:21 +0100

    Create new App and basic scaffolding of LogIn page inside the App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import history from "./history";
+import LogIn from "./Components/LogIn";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [pizza] = useState("Pepperoni");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>An amazing pizza rating App!</h1>
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component = {LogIn}/>
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/LogIn.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+
+const LogIn = () => {
+  const pizzaUser = {
+    tag: "Pizza Bro",
+    pizza: "Best Pizza any pizza"
+  };
+  const [user, setDetails] = useState(pizzaUser);
+  const [click, setClick] = useState(false);
+
+  const welcomeMsg = () => {
+    return (
+      <div>
+        Welcome: {user.tag} <br /> Let's look at: {user.pizza}
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <form>
+        <div className="form-group">
+          <label for="examplePizza">Pizza user Handle</label>
+          <input
+            type="text"
+            className="form-control"
+            id="examplePizza"
+            aria-describedby="pizzaHelp"
+            placeholder="Enter Pizza user Tag"
+            onChange={e => {
+              const tag = e.target.value;
+              setDetails(user => {
+                return { ...user, tag };
+              });
+            }}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We MIGHT share your pizza user tag (*hint* Pineapples are evil).
+          </small>
+        </div>
+        <div className="form-group">
+          <label for="examplePizzaType">Best Pizza</label>
+          <input
+            type="text"
+            className="form-control"
+            id="examplePizzaType"
+            placeholder="Pepperoni is good"
+            onChange={e => {
+              const pizza = e.target.value;
+              setDetails(user => {
+                return { ...user, pizza };
+              });
+            }}
+          />
+        </div>
+
+        {/* <button className="btn btn-primary" onClick={() => setClick(true)}>
+        Enter the App
+        { click ? welcomeMsg() : "Nope" }
+      </button> */}
+      </form>
+      <button className="btn btn-primary" onClick={() => setClick(true)}>
+        
+        {click ? welcomeMsg() : "Enter the App"}
+      </button>
+    </div>
+  );
+};
+
+export default LogIn;
+
+// Sandbox
+/*
+    const [newPizza, setPizza] = useState("Mexican Pizza!")
+    const specials = (val) => {
+        return (val +1)
+    }
+    return (
+        <div>
+            <h5>More Pizza Awaits!</h5>
+            <p>Starting off with {originalPizza}</p>
+            <h2>Brand new addition: { newPizza }! Only with {specials(2)}</h2>
+            <p>Or create your own! What will it be <input type="text" placeholder="Yum" onChange={e => setPizza(e.target.value)}></input></p>
+        </div>
+    )
+    */
