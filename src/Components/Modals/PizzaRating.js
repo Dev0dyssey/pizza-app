04640d43846a771f248838f30cd6d9d85c533a32
ThomasMartinUK <tmuk@protonmail.com>
2019-11-11 21:53:06 +0000

    Ability to create additional comments added through a document update inside the firestore database. Users are able to create additional comments and save them within the App

--- src/Components/Modals/PizzaRating.js
+++ src/Components/Modals/PizzaRating.js
@@ -1,7 +1,18 @@
-import React from "react";
-import '../../StyleSheets/modal.css'
+import React, { useState } from "react";
+import "../../StyleSheets/modal.css";
+import { db } from "../../base";
 
 const PizzaRating = props => {
+  // New comments state
+  const [comment, setComment] = useState([])
+
+  const handleSubmit = () => {
+    db.collection("pizza-collection").doc("D0BUAHcrFbuVJkIdMwYy")
+    .update({comments: comment})
+    .then(() => console.log("Comment updated"))
+    .catch(error => `Something went wrong: ${error}`)
+  }
+
   return (
     <>
       <div className="modal-dialog" role="document">
@@ -24,60 +35,69 @@ const PizzaRating = props => {
             <br />
             Rating:
             <div className="form-check form-check-inline ml-2 mb-3">
-            <label className="form-check-label mr-1" for="inlineRadio1">
-              1
-            </label>
-            <input
-              className="form-check-input mr-3"
-              type="radio"
-              name="inlineRadioOptions"
-              id="inlineRadio1"
-              value="option1"
-            />
-            <label className="form-check-label mr-1" for="inlineRadio2">
-              2
-            </label>
-            <input
-              className="form-check-input mr-3"
-              type="radio"
-              name="inlineRadioOptions"
-              id="inlineRadio2"
-              value="option2"
-            />
-            <label className="form-check-label mr-1" for="inlineRadio3">
-              3
-            </label>
-            <input
-              className="form-check-input mr-3"
-              type="radio"
-              name="inlineRadioOptions"
-              id="inlineRadio3"
-              value="option3"
-            />
-            <label className="form-check-label mr-1" for="inlineRadio3">
-              4
-            </label>
-            <input
-              className="form-check-input mr-3"
-              type="radio"
-              name="inlineRadioOptions"
-              id="inlineRadio3"
-              value="option3"
-            />
-            <label className="form-check-label mr-1" for="inlineRadio3">
-              5
-            </label>
-            <input
-              className="form-check-input mr-3"
-              type="radio"
-              name="inlineRadioOptions"
-              id="inlineRadio3"
-              value="option3"
-            />
+              <label className="form-check-label mr-1" for="inlineRadio1">
+                1
+              </label>
+              <input
+                className="form-check-input mr-3"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio1"
+                value="option1"
+              />
+              <label className="form-check-label mr-1" for="inlineRadio2">
+                2
+              </label>
+              <input
+                className="form-check-input mr-3"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio2"
+                value="option2"
+              />
+              <label className="form-check-label mr-1" for="inlineRadio3">
+                3
+              </label>
+              <input
+                className="form-check-input mr-3"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio3"
+                value="option3"
+              />
+              <label className="form-check-label mr-1" for="inlineRadio3">
+                4
+              </label>
+              <input
+                className="form-check-input mr-3"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio3"
+                value="option3"
+              />
+              <label className="form-check-label mr-1" for="inlineRadio3">
+                5
+              </label>
+              <input
+                className="form-check-input mr-3"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio3"
+                value="option3"
+              />
+            </div>
+            <div className="input-group">
+              <textarea 
+                className="form-control" 
+                placeholder="Comments"
+                onChange={ e => {
+                  const newComment = e.target.value;
+                  setComment(newComment)
+                }}
+              ></textarea>
+            </div>
           </div>
-          </div>
-          
-          
+
           <div className="modal-footer">
             <button
               type="button"
@@ -86,11 +106,15 @@ const PizzaRating = props => {
             >
               Close
             </button>
-            <button type="button" className="btn btn-primary" data-dismiss="modal">
-              Save changes
+            <button
+              type="button"
+              className="btn btn-primary"
+              data-dismiss="modal"
+              onClick={() => handleSubmit()}
+            >
+              Add Rating & Comment
             </button>
           </div>
-
         </div>
       </div>
     </>
