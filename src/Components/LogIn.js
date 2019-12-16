6aa263ab0e0ba732c2a91372df01c58fec00bcb2
ThomasMartinUK <tmuk@protonmail.com>
2019-12-16 21:32:26 +0000

    Added a new method to handle resetting a user password in <LogIn /> component. New modal element added to contain this method along with a conditional render of modal content based on the status of email being sent or not by the App

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -1,9 +1,13 @@
-import React, { useCallback, useContext } from "react";
+import React, { useState, useCallback, useContext } from "react";
 import { withRouter, Redirect, Link } from "react-router-dom";
 import app from "../base";
+import "../StyleSheets/landing.css"
 import { AuthContext } from "../Auth";
 
 const LogIn = ({ history }) => {
+  const [email, setEmail] = useState("")
+  const [emailStatus, changeStatus] = useState(false)
+
   const handleLogin = useCallback(
     async event => {
       event.preventDefault();
@@ -19,6 +23,18 @@ const LogIn = ({ history }) => {
     },
     [history]
   );
+
+  const resetPasswordEmail = () => {
+    console.log(`Reset Password email sent`)
+    app.auth().sendPasswordResetEmail(email)
+      .then(() => {
+        changeStatus(true);
+      })
+      .catch(err => {
+        console.log(`An error has occured`, err)
+      })
+
+  }
 
   const { currentUser } = useContext(AuthContext);
 
@@ -62,6 +78,42 @@ const LogIn = ({ history }) => {
             Sign up
           </button>
         </Link>
+        <div className="resetBtn" style={{ hover: "pointer" }}>
+          <span data-toggle="modal" data-target="#resetPasswordModal">Forgot your password?</span>
+        </div>
+        <div className="modal" id="resetPasswordModal" tabIndex="-1" role="dialog">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <div className="modal-title"> Reset Password Request</div>
+              </div>
+              <div className="modal-body">
+                <div className="inputGroup">
+                  {emailStatus &&
+                    <div>
+                      <p>A password reset link was sent to the email address you entered!</p>
+                    </div>
+                  }
+                  {!emailStatus &&
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Please enter the email you used to register"
+                      onChange={e => {
+                        const resetEmail = e.target.value;
+                        setEmail(resetEmail)
+                      }}
+                    ></input>
+                  }
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={() => changeStatus(false)}>Cancel</button>
+                <button type="button" className="btn btn-primary" onClick={() => resetPasswordEmail()}>Reset Password</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
