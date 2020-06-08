35bb0d9ced72a033eca8bedf86f1e3057caf2f5e
ThomasMartinUK <tmuk@protonmail.com>
2020-06-08 19:48:21 +0100

    Update NPM packages used inside the App

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../StyleSheets/landing.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../StyleSheets/landing.css";
 
 const Landing = () => {
-    return (
-        <div className="bgImg">
-            <div className="jumbotron background-pizza" style={{ textAlign: "center", marginTop: "25vh", verticalAlign: "baseline"}}>
-                <h1>PIZZA RATE</h1>
-                <Link to = {`/login`}>
-                    <btn className="btn btn-primary" style={{marginTop: "0.5rem"}}>ENTER</btn>
-                </Link>
-                <br/>
-                <br/>
-                <p>Please explore and test the App!</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="bgImg">
+      <div
+        className="jumbotron background-pizza"
+        style={{
+          textAlign: "center",
+          marginTop: "25vh",
+          verticalAlign: "baseline",
+        }}
+      >
+        <h1>PIZZA RATE</h1>
+        <Link to={`/login`}>
+          <btn className="btn btn-primary" style={{ marginTop: "0.5rem" }}>
+            ENTER
+          </btn>
+        </Link>
+        <br />
+        <br />
+        <p>Please explore and test the App!</p>
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
