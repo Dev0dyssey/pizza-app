155adc0c49f48200e94b9abd1cc9a4e63f45cbab
Thomas Andrew Martin <47917727+Dev0dyssey@users.noreply.github.com>
2022-08-23 19:13:32 +0100

    Added Capacitor support

---
+++ android/app/src/main/java/com/vrate/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vrate.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vrate.app',
+  appName: '[name]',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
